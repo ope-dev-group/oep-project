29436b57f05c57b97112adff8fa54748ac559765
kezhixiong <349345707@qq.com>
2018-11-19 09:49:41 +0800

    修改包结构

--- qloudbiz-api-product/src/main/java/com/csft/product/endpoint/ProductMaterialsServiceEndpoint.java
+++ qloudbiz-api-product/src/main/java/com/csft/product/endpoint/ProductMaterialsServiceEndpoint.java
@@ -1,4 +1,4 @@
-package com.csft.product.sdk;
+package com.csft.product.endpoint;
 
 
 import java.io.BufferedReader;
@@ -20,32 +20,31 @@ import com.qloudfin.qloudbus.reactive.Callback;
 
 
 /**
- * 产品单位微服务
- *产品单位是产品销售的基本单元，例如一台、一箱，一箱=4台，订单可能按照台或者按照箱进行销售。产品单位之间有固定的换算关系。
- *一般来说，产品单位是针对主物料而言的，产品SKU属性不同，销售单位一般不会有变化。
- *由于产品单位与产品主物料有强烈的关联关系，二者之间是多对多关系，一般都是产品主物料创建完成后，再创建相关的产品单位。
+ * 产品线服务
+ *
+ * @author Kezx
  *
  */
 @RequestMapping("/products")
-public class ProductUnitsServiceEndpoint {
+public class ProductMaterialsServiceEndpoint {
 	
-	private final static Logger logger=LoggerFactory.getLogger(ProductUnitsServiceEndpoint.class);
+	private final static Logger logger=LoggerFactory.getLogger(ProductMaterialsServiceEndpoint.class);
 	
 	/**
-	 * 添加产品单位
+	 * 添加产品线
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/units",method=RequestMethod.POST)
-	public void addUnits(Callback<Object> callback,Map<String,String> body){
+	@RequestMapping(value="/materials",method=RequestMethod.POST)
+	public void addMaterials(Callback<Object> callback,Map<String,String> body){
 		
 		
 		//调试日志
-		logger.debug(">>>>>>>>>>>>>Add Product Units param is:{}",body);
+		logger.debug(">>>>>>>>>>>>>Add materials param is:{}",body);
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
 		
 		
 		
@@ -68,6 +67,7 @@ public class ProductUnitsServiceEndpoint {
 			isr.close();
 			in.close();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -81,20 +81,20 @@ public class ProductUnitsServiceEndpoint {
 	
 	
 	/**
-	 * 修改产品单位
+	 * 修改产品线
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/units/{unitId}",method=RequestMethod.PUT)
-	public void updateUnits(Callback<Object> callback,@PathVariable("unitId") String unitId,Map<String,String> body){
+	@RequestMapping(value="/materials/{lineId}",method=RequestMethod.PUT)
+	public void updateMaterials(Callback<Object> callback,@PathVariable("lineId") String lineId,Map<String,String> body){
 		
 		
 		//调试日志
-		logger.debug(">>>>>>>>>Update units :unitId is:{},param is {}",unitId,body);
+		logger.debug(">>>>>>>>>Update materials :materialId is:{},param is {}",lineId,body);
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
 		
 		
 		
@@ -130,20 +130,20 @@ public class ProductUnitsServiceEndpoint {
 	}
 	
 	/**
-	 * 删除产品单位
+	 * 删除产品线
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/units/{unitId}",method=RequestMethod.DELETE)
-	public void deleteUnits(Callback<Object> callback,@PathVariable("unitId") String unitId){
+	@RequestMapping(value="/materials/{lineId}",method=RequestMethod.DELETE)
+	public void deleteMaterials(Callback<Object> callback,@PathVariable("lineId") String lineId){
 		
 		
 		//调试日志
-		logger.debug(">>>>>>>>>>>>>>>>Delete units :unitId is:{}",unitId);
+		logger.debug("Delete materials :materialId is:{}",lineId);
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
 		
 		
 		
@@ -179,20 +179,70 @@ public class ProductUnitsServiceEndpoint {
 	}
 	
 	/**
-	 * 查询产品单位
+	 * 查询产品线列表信息
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/units/products/{productId}",method=RequestMethod.GET)
-	public void queryUnit(Callback<Object> callback,@PathVariable("productId") String  productId){
+	@RequestMapping(value="/materials",method=RequestMethod.GET)
+	public void queryMaterials(Callback<Object> callback){
 		
 		
 		//调试日志
-		logger.debug(">>>>>>>>>>>>>>Query  Product Unit by productId,productId is {}",productId);
+		logger.debug("Query materials");
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
+		
+		
+		
+		StringBuffer sb=new StringBuffer();
+		InputStreamReader isr=null;
+		try {
+			isr = new InputStreamReader(in,"UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		BufferedReader reader=new BufferedReader(isr);
+		String  line=null;
+		try {
+			while((line=reader.readLine())!=null){
+				sb.append(line);
+				
+			}
+			reader.close();
+			isr.close();
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//业务处理
+		sb.toString().replaceAll("\t","");
+		Object resultObj=JSON.parse(sb.toString());
+	
+		
+		callback.accept(resultObj);	
+	}
+	
+	
+	/**
+	 * 查询产品线列表信息
+	 * @param callback
+	 * @param corpId
+	 * @param token
+	 */
+	@RequestMapping(value="/materials/{materialId}",method=RequestMethod.GET)
+	public void queryMaterial(Callback<Object> callback,@PathVariable("materialId")String materialId){
+		
+		
+		//调试日志
+		logger.debug(">>>>>>>Query material Detail materialId is {}",materialId);
+		
+		//加载数据文件
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
 		
 		
 		
--- qloudbiz-api-product/src/main/java/com/csft/product/endpoint/ProductUnitsServiceEndpoint.java
+++ qloudbiz-api-product/src/main/java/com/csft/product/endpoint/ProductUnitsServiceEndpoint.java
@@ -1,4 +1,4 @@
-package com.csft.product.sdk;
+package com.csft.product.endpoint;
 
 
 import java.io.BufferedReader;
@@ -20,31 +20,32 @@ import com.qloudfin.qloudbus.reactive.Callback;
 
 
 /**
- * 产品线服务
- *
- * @author Kezx
+ * 产品单位微服务
+ *产品单位是产品销售的基本单元，例如一台、一箱，一箱=4台，订单可能按照台或者按照箱进行销售。产品单位之间有固定的换算关系。
+ *一般来说，产品单位是针对主物料而言的，产品SKU属性不同，销售单位一般不会有变化。
+ *由于产品单位与产品主物料有强烈的关联关系，二者之间是多对多关系，一般都是产品主物料创建完成后，再创建相关的产品单位。
  *
  */
 @RequestMapping("/products")
-public class ProductMaterialsServiceEndpoint {
+public class ProductUnitsServiceEndpoint {
 	
-	private final static Logger logger=LoggerFactory.getLogger(ProductMaterialsServiceEndpoint.class);
+	private final static Logger logger=LoggerFactory.getLogger(ProductUnitsServiceEndpoint.class);
 	
 	/**
-	 * 添加产品线
+	 * 添加产品单位
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/materials",method=RequestMethod.POST)
-	public void addMaterials(Callback<Object> callback,Map<String,String> body){
+	@RequestMapping(value="/units",method=RequestMethod.POST)
+	public void addUnits(Callback<Object> callback,Map<String,String> body){
 		
 		
 		//调试日志
-		logger.debug(">>>>>>>>>>>>>Add materials param is:{}",body);
+		logger.debug(">>>>>>>>>>>>>Add Product Units param is:{}",body);
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
 		
 		
 		
@@ -67,7 +68,6 @@ public class ProductMaterialsServiceEndpoint {
 			isr.close();
 			in.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -81,20 +81,20 @@ public class ProductMaterialsServiceEndpoint {
 	
 	
 	/**
-	 * 修改产品线
+	 * 修改产品单位
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/materials/{lineId}",method=RequestMethod.PUT)
-	public void updateMaterials(Callback<Object> callback,@PathVariable("lineId") String lineId,Map<String,String> body){
+	@RequestMapping(value="/units/{unitId}",method=RequestMethod.PUT)
+	public void updateUnits(Callback<Object> callback,@PathVariable("unitId") String unitId,Map<String,String> body){
 		
 		
 		//调试日志
-		logger.debug(">>>>>>>>>Update materials :materialId is:{},param is {}",lineId,body);
+		logger.debug(">>>>>>>>>Update units :unitId is:{},param is {}",unitId,body);
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
 		
 		
 		
@@ -130,20 +130,20 @@ public class ProductMaterialsServiceEndpoint {
 	}
 	
 	/**
-	 * 删除产品线
+	 * 删除产品单位
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/materials/{lineId}",method=RequestMethod.DELETE)
-	public void deleteMaterials(Callback<Object> callback,@PathVariable("lineId") String lineId){
+	@RequestMapping(value="/units/{unitId}",method=RequestMethod.DELETE)
+	public void deleteUnits(Callback<Object> callback,@PathVariable("unitId") String unitId){
 		
 		
 		//调试日志
-		logger.debug("Delete materials :materialId is:{}",lineId);
+		logger.debug(">>>>>>>>>>>>>>>>Delete units :unitId is:{}",unitId);
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
 		
 		
 		
@@ -179,70 +179,20 @@ public class ProductMaterialsServiceEndpoint {
 	}
 	
 	/**
-	 * 查询产品线列表信息
+	 * 查询产品单位
 	 * @param callback
 	 * @param corpId
 	 * @param token
 	 */
-	@RequestMapping(value="/materials",method=RequestMethod.GET)
-	public void queryMaterials(Callback<Object> callback){
+	@RequestMapping(value="/units/products/{productId}",method=RequestMethod.GET)
+	public void queryUnit(Callback<Object> callback,@PathVariable("productId") String  productId){
 		
 		
 		//调试日志
-		logger.debug("Query materials");
+		logger.debug(">>>>>>>>>>>>>>Query  Product Unit by productId,productId is {}",productId);
 		
 		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
-		
-		
-		
-		StringBuffer sb=new StringBuffer();
-		InputStreamReader isr=null;
-		try {
-			isr = new InputStreamReader(in,"UTF-8");
-		} catch (UnsupportedEncodingException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		BufferedReader reader=new BufferedReader(isr);
-		String  line=null;
-		try {
-			while((line=reader.readLine())!=null){
-				sb.append(line);
-				
-			}
-			reader.close();
-			isr.close();
-			in.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//业务处理
-		sb.toString().replaceAll("\t","");
-		Object resultObj=JSON.parse(sb.toString());
-	
-		
-		callback.accept(resultObj);	
-	}
-	
-	
-	/**
-	 * 查询产品线列表信息
-	 * @param callback
-	 * @param corpId
-	 * @param token
-	 */
-	@RequestMapping(value="/materials/{materialId}",method=RequestMethod.GET)
-	public void queryMaterial(Callback<Object> callback,@PathVariable("materialId")String materialId){
-		
-		
-		//调试日志
-		logger.debug(">>>>>>>Query material Detail materialId is {}",materialId);
-		
-		//加载数据文件
-		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-create.json");		
+		InputStream in=ClassLoader.getSystemResourceAsStream("com/qloudfin/qloudbiz/apidef/products/product-unit-create.json");		
 		
 		
 		
