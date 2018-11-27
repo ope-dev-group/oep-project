d15ebc51faa24e37da593c93e263014e9c454b30
StrongLucy <lcylcylcy360@gmail.com>
2018-11-27 11:20:56 +0800

    test

---
+++ qloudbiz-api-product/src/main/java/com/qloudbiz/product/endpoint/ProductLogicTypesServiceEndpoint.java
@@ -0,0 +1,28 @@
+package com.qloudbiz.product.endpoint;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSON;
+import com.qloudfin.qloudbus.annotation.PathVariable;
+import com.qloudfin.qloudbus.annotation.RequestMapping;
+import com.qloudfin.qloudbus.annotation.RequestMethod;
+import com.qloudfin.qloudbus.annotation.RequestParam;
+import com.qloudfin.qloudbus.reactive.Callback;
+
+
+
+@RequestMapping("/products")
+public class ProductLogicTypesServiceEndpoint {
+	
+	
+	
+}
